88a2208999cba5b1caf85d0535867c4296c070b3
anggapur <arimbawaangga@gmail.com>
2017-07-19 23:20:55 -0700

    keranjang codingan tambah, delete,edit

--- app/Http/Controllers/cartCtrl.php
+++ app/Http/Controllers/cartCtrl.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\cart;
 use jsonPrint;
 use DB;
+use Validator;
 class cartCtrl extends Controller
 {
     /**
@@ -38,6 +39,36 @@ class cartCtrl extends Controller
     public function store(Request $request)
     {
         //
+        $data['cart_buyer_id'] = $request->input('user_id');
+        $data['cart_product_id'] = $request->input('product_id');
+        $data['cart_qty'] = $request->input('qty');
+        if($data['cart_qty'] == "")
+            $data['cart_qty'] = "1";
+
+        $query = cart::where([
+                'cart_buyer_id' => $request->input('user_id'),
+                'cart_product_id' => $request->input('product_id'),
+            ]);
+        if(count($query->get()) == 0)
+        {
+            if(cart::create($data))
+            {
+                $feedback['message'] = 'success';
+                $feedback['action'] = 'store';
+                $feedback['data'] = $query->select(DB::raw('carts.*,products.*,(carts.cart_qty*products.product_sell_price) as subtotal'))
+                        ->leftJoin('products','carts.cart_product_id','=','products.product_id')
+                        ->with(['seller'=>function($query){
+                            $query->select('seller_id','seller_name');
+                        }])->first();
+            }
+        }
+        else
+        {
+            $feedback['message'] = 'data is exists';
+            $feedback['action'] = 'store';            
+        }
+
+        return jsonPrint::prints($feedback);
     }
 
     /**
@@ -84,7 +115,66 @@ class cartCtrl extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       //  //proses
+       //  $data['cart_product_id'] = $id;
+       //  $data['cart_buyer_id'] = $request->input('user_id');
+       //  $qty = $request->input('qty');
+
+       //  //validasi 
+       // $validator = Validator::make($request->all(),[
+       //          'user_id' => 'required',
+       //          'product_id' => 'required',                
+       //      ]);
+       //  if($validator->fails())
+       //  {   
+       //      $feedback['action'] ='update';
+       //      $feedback['message'] = 'failed';
+       //      $feedback['errors'] =$validator->errors();
+       //      return $feedback;
+       //  }
+
+       //  if($qty == "")
+       //      $qty = "1";
+        
+       //  $query = cart::where($data); 
+       //  if(count($query->get()) > 0)
+       //  {
+       //      $exe = $query->update(['cart_qty'=>$qty]);
+       //      if($exe)
+       //      {
+       //          $feedback['message'] = "success";
+       //          $feedback['action'] = "update";
+       //          $feedback['data'] = $query->leftJoin('products','carts.cart_product_id','=','products.product_id')
+       //                              ->with(['seller'=>function($query){
+       //                                      $query->select('seller_id','seller_name');
+       //                                  }])
+       //                              ->first();
+       //      }
+       //      else
+       //      {
+       //          $feedback['message'] = "failed";
+       //      }
+       //  }
+
+
+        if(cart::where('cart_id',$id)->update(['cart_qty' => $request->input('qty')]))
+        {
+            $feedback['message'] = 'success';
+            $feedback['action'] = 'update';
+            $feedback['data'] = cart::where('cart_id',$id)
+                                ->leftJoin('products','carts.cart_product_id','=','products.product_id')
+                                    ->with(['seller'=>function($query){
+                                            $query->select('seller_id','seller_name');
+                                        }])
+                                    ->first();
+            
+        }
+        else
+        {
+            $feedback['message'] = 'failed';
+            $feedback['desc'] = 'the data is not exist or somthing else';
+        }
+        return jsonPrint::prints($feedback);
     }
 
     /**
@@ -93,9 +183,49 @@ class cartCtrl extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id,Request $request)
     {
         //
-        return $id;
+        // $user_id = $request->input('user_id');
+        // //find the data base on what we get
+        // $query  = cart::where([
+        //         'cart_product_id' => $id,
+        //         'cart_buyer_id' => $user_id,
+        //     ]);        
+        // $data['total'] = count($query->get());
+        // $data['data'] = $query
+        //             ->leftJoin('products','carts.cart_product_id','=','products.product_id')
+        //             ->with(['seller'=>function($query){
+        //                     $query->select('seller_id','seller_name');
+        //                 }])
+        //             ->first();
+        // //proses delete;
+
+        // if($query->delete())
+        //     $data['message'] = "success";            
+        // else
+        // {
+        //     $data['message'] = "failed";
+        //     $data['desc'] = "The Data No Exist or Something Else";
+        // }
+
+        // return $data;
+        $query = cart::where('cart_id',$id);
+        if(count($query->get()) > 0)
+        {
+            $exe = $query->delete();
+            if($exe)
+            {
+                $feedback['message'] = 'success';
+                $feedback['action'] ='delete';
+            }
+        }
+        else
+        {
+            $feedback['message'] = 'failed';
+            $feedback['action'] ='delete';
+            $feedback['desc'] = 'data not exists';
+        }
+        return jsonPrint::prints($feedback);
     }
 }
--- app/cart.php
+++ app/cart.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class cart extends Model
 {
     //
+    protected $fillable = ['cart_buyer_id','cart_qty','cart_product_id'];
     public function seller()
     {
     	return $this->hasOne('App\seller','seller_id','product_seller_id');
